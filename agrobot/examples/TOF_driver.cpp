44050ddec743386f30415026da6956587bc43b29
bja1701 <brightdex17@gmail.com>
2024-12-16 16:04:10 -0700

    fixed small bug in TOF_driver

--- agrobot/examples/TOF_driver.cpp
+++ agrobot/examples/TOF_driver.cpp
@@ -4,14 +4,14 @@
 #define EN1       2                      // EN pin for left TMF8801
 #define EN2       3                      // EN pin for right TMF8801
 #define EN3       4                      // EN pin for front TMF8801
-#define EN4       5                      // EN pin for back TMF8801
+// #define EN4       5                      // EN pin for back TMF8801
 #define INT      -1                      // INT pin is floating, not used in this demo
 
 // Create two sensor objects, one for each TMF8801
 DFRobot_TMF8801 tofLeft(/*enPin =*/EN1, /*intPin=*/INT);
 DFRobot_TMF8801 tofRight(/*enPin =*/EN2, /*intPin=*/INT);
 DFRobot_TMF8801 tofFront(/*enPin =*/EN3, /*intPin=*/INT);
-DFRobot_TMF8801 tofBack(/*enPin =*/EN4, /*intPin=*/INT);
+// DFRobot_TMF8801 tofBack(/*enPin =*/EN4, /*intPin=*/INT);
 
 // Calibration data
 uint8_t caliDataBuf[14] = {0x41,0x57,0x01,0xFD,0x04,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x04};
@@ -25,13 +25,13 @@ void setup() {
   pinMode(EN1, OUTPUT);
   pinMode(EN2, OUTPUT);
   pinMode(EN3, OUTPUT);
-  pinMode(EN4, OUTPUT);
+  // pinMode(EN4, OUTPUT);
   
   // Initially set all EN pins LOW
   digitalWrite(EN1, LOW);
   digitalWrite(EN2, LOW);
   digitalWrite(EN3, LOW);
-  digitalWrite(EN4, LOW);
+  // digitalWrite(EN4, LOW);
   delay(50); // Give some time for sensors to reset
 
   // Initialize first sensor (left)
@@ -102,8 +102,8 @@ void setup() {
   }
   Serial.println("done.");
   
-  // Set I2C address for second sensor
-  tofRight.setI2CAddress(3);  // Set to address 3
+  // Set I2C address for third sensor
+  tofFront.setI2CAddress(3);  // Set to address 3
 
   // Print second sensor info
   Serial.println("Front Sensor:");
@@ -114,28 +114,28 @@ void setup() {
   Serial.print("Model: ");
   Serial.println(tofFront.getSensorModel());
 
-  // Initialize fourth sensor (back)
-  digitalWrite(EN4, HIGH);  // Enable back sensor
-  delay(50);                // Wait for the sensor to power up
+  // // Initialize fourth sensor (back)
+  // digitalWrite(EN4, HIGH);  // Enable back sensor
+  // delay(50);                // Wait for the sensor to power up
 
-  Serial.print("Initializing back TMF8801 sensor...");
-  while(tofBack.begin() != 0) {
-    Serial.println("failed.");
-    delay(1000);
-  }
-  Serial.println("done.");
+  // Serial.print("Initializing back TMF8801 sensor...");
+  // while(tofBack.begin() != 0) {
+  //   Serial.println("failed.");
+  //   delay(1000);
+  // }
+  // Serial.println("done.");
   
-  // Set I2C address for second sensor
-  tofBack.setI2CAddress(4);  // Set to address 3
+  // // Set I2C address for fourth sensor
+  // tofBack.setI2CAddress(4);  // Set to address 3
 
-  // Print second sensor info
-  Serial.println("Back Sensor:");
-  Serial.print("Software Version: ");
-  Serial.println(tofBack.getSoftwareVersion());
-  Serial.print("Unique ID: ");
-  Serial.println(tofBack.getUniqueID(), HEX);
-  Serial.print("Model: ");
-  Serial.println(tofBack.getSensorModel());
+  // // Print Fourth sensor info
+  // Serial.println("Back Sensor:");
+  // Serial.print("Software Version: ");
+  // Serial.println(tofBack.getSoftwareVersion());
+  // Serial.print("Unique ID: ");
+  // Serial.println(tofBack.getUniqueID(), HEX);
+  // Serial.print("Model: ");
+  // Serial.println(tofBack.getSensorModel());
 
 
 
@@ -143,13 +143,13 @@ void setup() {
   tofLeft.setCalibrationData(caliDataBuf, sizeof(caliDataBuf));
   tofRight.setCalibrationData(caliDataBuf, sizeof(caliDataBuf));
   tofFront.setCalibrationData(caliDataBuf, sizeof(caliDataBuf));
-  tofBack.setCalibrationData(caliDataBuf, sizeof(caliDataBuf));
+  // tofBack.setCalibrationData(caliDataBuf, sizeof(caliDataBuf));
 
   // Start measurements on both sensors
   tofLeft.startMeasurement(/*cailbMode =*/tofLeft.eModeCalib);
   tofRight.startMeasurement(/*cailbMode =*/tofRight.eModeCalib);
   tofFront.startMeasurement(/*cailbMode =*/tofFront.eModeCalib);
-  tofBack.startMeasurement(/*cailbMode =*/tofBack.eModeCalib);
+  // tofBack.startMeasurement(/*cailbMode =*/tofBack.eModeCalib);
 }
 
 void loop() {
@@ -173,13 +173,13 @@ void loop() {
     Serial.print(" mm    ");
   }
 
-  if (tofBack.isDataReady()) {
-    Serial.print("Back Distance = ");
-    Serial.print(tofBack.getDistance_mm());
-    Serial.println(" mm");
-  }
+  // if (tofBack.isDataReady()) {
+  //   Serial.print("Back Distance = ");
+  //   Serial.print(tofBack.getDistance_mm());
+  //   Serial.println(" mm");
+  // }
 
   delay(50); // Adjust delay to control the measurement rate
 }
 
-//TODO: add ability to use 4 sensors
+// TODO: figure out why teensy can't power 4 sensors at the same time, then uncomment all the back sensor code
