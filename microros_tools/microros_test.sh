20eff44be97b2273ac151f9ed6bc27fd2487162d
Nelson Durrant <snelsondurrant@gmail.com>
2025-02-24 09:08:30 -0700

    Tab updates

--- microros_tools/microros_test.sh
+++ microros_tools/microros_test.sh
@@ -5,32 +5,32 @@
 # - Use this after setting up a new PCB to test the agent and Teensy board connections
 
 function printInfo {
-  echo -e "\033[0m\033[36m[INFO] $1\033[0m"
+  	echo -e "\033[0m\033[36m[INFO] $1\033[0m"
 }
 
 function printWarning {
-  echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
+  	echo -e "\033[0m\033[33m[WARNING] $1\033[0m"
 }
 
 function printError {
-  echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
+  	echo -e "\033[0m\033[31m[ERROR] $1\033[0m"
 }
 
 cleanup() {
-  killall micro_ros_agent
-  wait
-  exit 0
+	killall micro_ros_agent
+	wait
+	exit 0
 }
 trap cleanup SIGINT
 
 if [ -z "$(tycmd list | grep Teensy)" ]; then
-  printError "No Teensy boards avaliable to connect to"
-  exit 1
+  	printError "No Teensy boards avaliable to connect to"
+  	exit 1
 else 
-  source ~/microros_ws/install/setup.bash
-  ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 -b 6000000 &
+  	source ~/microros_ws/install/setup.bash
+  	ros2 run micro_ros_agent micro_ros_agent serial --dev /dev/ttyACM0 -b 6000000 &
 
-  sleep 5
+  	sleep 5
 fi
 
 source ~/ros2_ws/install/setup.bash
